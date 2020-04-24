3a063e49934f40bc99d5a83f9267cbdffe9bdfdd
David Ambe <davidambe@outlook.com>
2020-04-24 15:26:09 +0100

    Added TDD3

---
+++ src/main/java/com/qa/testdrivendevelopment/Runner.java
@@ -0,0 +1,37 @@
+package com.qa.testdrivendevelopment;
+
+import java.util.Scanner;
+
+public class Runner {
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * // Two dimensional Array int twoArrays(int x, int y) {
+ * 
+ * while(x > 0 || y > 0) { int[] myArray = new int[2];
+ * 
+ * }
+ * 
+ * System.out.println(values + "Please Enter values greater than 0: "); return x
+ * + y;
+ * 
+ * }
+ */
---
+++ src/main/java/com/qa/testdrivendevelopment/mainrunner.java
@@ -0,0 +1,13 @@
+package com.qa.testdrivendevelopment;
+
+import java.util.stream.Stream;
+
+public class mainrunner {
+	
+
+	public static void main(String[] args) {
+		int num = 5;
+		char s1, s2, s3, s4;
+		System.out.printf("%d + %d%d  + %d%d%d + %d%d%d%d", num, num, num, num);
+	}
+}
